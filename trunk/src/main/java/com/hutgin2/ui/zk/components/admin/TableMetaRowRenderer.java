f235e18681efae01bedced49beafffd594800e82
d.alexei.gubanov@gmail.com <d.alexei.gubanov@gmail.com@be01c5d7-7ea6-e2ce-f8ae-e0ce93b5f565>
2013-06-20 08:40:43 +0000

    modal ok

--- trunk/src/main/java/com/hutgin2/ui/zk/components/admin/TableMetaRowRenderer.java
+++ trunk/src/main/java/com/hutgin2/ui/zk/components/admin/TableMetaRowRenderer.java
@@ -1,20 +1,17 @@
 package com.hutgin2.ui.zk.components.admin;
 
 import com.hutgin2.core.meta.TableMeta;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.EventListener;
-import org.zkoss.zul.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.RowRenderer;
 
 public class TableMetaRowRenderer implements RowRenderer<TableMeta> {
 
-    private Grid tablesList;
+    private TablesController.EventListenerBuilder onDoubleClick;
 
-    public TableMetaRowRenderer(Grid tablesList) {
-        this.tablesList = tablesList;
+    public TableMetaRowRenderer(TablesController.EventListenerBuilder onDoubleClick) {
+        this.onDoubleClick = onDoubleClick;
     }
 
     @Override
@@ -22,20 +19,7 @@ public class TableMetaRowRenderer implements RowRenderer<TableMeta> {
         row.appendChild(new Checkbox());
         row.appendChild(new Label(table.getName()));
         row.appendChild(new Label(table.getTableName()));
-        row.addEventListener("onDoubleClick", new EventListener<Event>() {
-            @Override
-            public void onEvent(Event event) throws Exception {
-                //create a window programmatically and use it as a modal dialog.
-                Map<String, Object> params = new HashMap<>();
-                params.put("action", "edit");
-                params.put("entity", table);
-                params.put("grid", tablesList);
-
-                Window window = (Window) Executions.createComponents(
-                        "/zul/components/admin/tableForm.zul", null, params);
-                window.doModal();
-            }
-        });
+        row.addEventListener("onDoubleClick", onDoubleClick.getEventListener(table));
 
     }
 }
